267501179c4d39b2ded2e614de1207c4735c5e35
Brett <amberg48@students.rowan.edu>
2022-04-28 11:12:43 -0400

    Created tracker view to render brewery information from API on webpage

--- tx_brewery_tracker/tracker/views.py
+++ tx_brewery_tracker/tracker/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponse
+import requests
 
 def tracker(request):
-    return render(request, 'tracker.html')
+    response = requests.get("https://api.openbrewerydb.org/breweries?by_state=texas")
+    package = response.json()
+    b = Brewery.objects.filter(pk=package[0]['id'])
+    if b:
+        print(b)
+    else:
+        print("Not in database")
+    return render(request, 'tracker.html', {'package' : package})
