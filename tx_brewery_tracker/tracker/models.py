afc10f95b1a19f7e4371ea94f619ccb2eca00af8
Brett <amberg48@students.rowan.edu>
2022-04-28 11:38:44 -0400

    Edited model to allow Null values

--- tx_brewery_tracker/tracker/models.py
+++ tx_brewery_tracker/tracker/models.py
@@ -5,18 +5,18 @@ class Brewery(models.Model):
     # Fields based on output from Open Brewery API
     id = models.CharField(primary_key=True, max_length=255)
     name = models.CharField(max_length=255)
-    type = models.CharField(max_length=100, blank=True)
-    street = models.CharField(max_length=255, blank=True)
-    address_2 = models.CharField(max_length=255, blank=True)
-    address_3 = models.CharField(max_length=255, blank=True)
-    city = models.CharField(max_length=100, blank=True)
-    state = models.CharField(max_length=100) # Should always equal 'Texas'
-    county_province = models.CharField(max_length=100, blank=True)
-    postal_code = models.CharField(max_length=100, blank=True)
-    country = models.CharField(max_length=100) # Should always equal 'United States'
-    longitude = models.CharField(max_length=100, blank=True)
-    latitude = models.CharField(max_length=100, blank=True)
-    phone = models.CharField(max_length=100, blank=True)
-    website_url = models.CharField(max_length=100, blank=True)
-    updated_at = models.CharField(max_length=100, blank=True) 
-    created_at = models.CharField(max_length=100, blank=True)
+    type = models.CharField(max_length=100, null=True)
+    street = models.CharField(max_length=255, null=True)
+    address_2 = models.CharField(max_length=255, null=True)
+    address_3 = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=100, null=True)
+    state = models.CharField(max_length=100, null=True) # Should always equal 'Texas'
+    county_province = models.CharField(max_length=100, null=True)
+    postal_code = models.CharField(max_length=100, null=True)
+    country = models.CharField(max_length=100, null=True) # Should always equal 'United States'
+    longitude = models.CharField(max_length=100, null=True)
+    latitude = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    website_url = models.CharField(max_length=100, null=True)
+    updated_at = models.CharField(max_length=100, null=True) 
+    created_at = models.CharField(max_length=100, null=True)
